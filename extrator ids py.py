210b9c3f9188642344fa725ffd58a550b1e6dac9
eduardo <102316412+eduardovidasbr@users.noreply.github.com>
2022-05-18 20:24:54 -0400

    Add files via upload
    Extrator de IDS publica Scrapping com python, Modulos usados requests, joblib, Parallel, delayed
    Feito para fim de estudos

---
+++ extrator ids py.py
@@ -0,0 +1,20 @@
+
+
+import requests
+from joblib import Parallel, delayed
+
+def multi_thread(tempo):
+        proxies = {'https': ''}
+        get =requests.get(f'https://paytrace.com/verify.pay?id={tempo}&mid=888000002887&img=5', proxies=proxies,verify=True)
+        resposta = get.text
+        if 'This account is closed.' in resposta:
+            print(f'ID Conta Fechada {tempo}')
+        
+        else:
+            if '<a href=></a>' in resposta:
+                print ('Vazio {}'.format(tempo))
+                with open("contas.txt", 'a') as arquivo:
+                    arquivo.write(f'Id da URL é {tempo} \n')
+
+resultado = Parallel(n_jobs=2)(delayed(multi_thread)(tempo) for tempo in range(0,2000000))
+
